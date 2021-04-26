ed2974dea1c5c62ce26bc8d8fbd6cf5ea9eaa1c4
Felipe Kunzler <felipe.kunzler@sap.com>
2021-04-26 20:43:09 -0300

    Subscribe for place order message in order service

--- order/src/main/java/com/k8s/poc/order/order/OrderApplication.java
+++ order/src/main/java/com/k8s/poc/order/order/OrderApplication.java
@@ -1,19 +1,39 @@
 package com.k8s.poc.order.order;
 
+import io.nats.client.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class OrderApplication implements CommandLineRunner {
+
+    @Resource
+    private Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+    private static final Logger LOG = LoggerFactory.getLogger(OrderApplication.class);
+
     @Override
-    public void run(String... args) {
-        System.out.println("Hello");
+    public void run(String... args) throws Exception {
+        String natsUrl = environment.getProperty("NATS_URL", Options.DEFAULT_URL);
+        Connection nc = Nats.connect(natsUrl);
+
+        Dispatcher d = nc.createDispatcher((msg) -> {
+        });
+        Subscription s = d.subscribe("placeOrder", (msg) -> {
+            String response = new String(msg.getData(), StandardCharsets.UTF_8);
+            LOG.info("Message received: " + response);
+        });
     }
 
 }
